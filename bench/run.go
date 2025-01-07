818c86f5e35c0511aeb7cf1dc4a380ed763cbfdf
mlange-42 <martin_lange_@gmx.net>
2025-01-07 22:12:28 +0100

    write markdown tables and info

--- bench/run.go
+++ bench/run.go
@@ -1,8 +1,12 @@
 package bench
 
 import (
+	"fmt"
 	"log"
 	"os"
+	"path"
+	"strings"
+	"time"
 
 	addremove "github.com/mlange-42/go-ecs-benchmarks/bench/add_remove"
 	"github.com/mlange-42/go-ecs-benchmarks/bench/create2comp"
@@ -10,11 +14,14 @@ import (
 	"github.com/mlange-42/go-ecs-benchmarks/bench/query2comp"
 	"github.com/mlange-42/go-ecs-benchmarks/bench/query32arch"
 	"github.com/mlange-42/go-ecs-benchmarks/bench/util"
+	"github.com/shirou/gopsutil/cpu"
 )
 
 func RunAll() {
-	err := os.Mkdir("results", os.ModePerm)
-	if err != nil && !os.IsExist(err) {
+	if err := os.Mkdir("results", os.ModePerm); err != nil && !os.IsExist(err) {
+		log.Fatal(err)
+	}
+	if err := writeInfo(); err != nil {
 		log.Fatal(err)
 	}
 
@@ -24,4 +31,23 @@ func RunAll() {
 
 	util.RunBenchmarks("create2comp.csv", create2comp.Benchmarks(), util.ToCSV)
 	util.RunBenchmarks("add_remove.csv", addremove.Benchmarks(), util.ToCSV)
+}
+
+func writeInfo() error {
+	text := strings.Builder{}
+	fmt.Fprintf(&text, "Last run: %s  \n", time.Now().Format(time.RFC1123))
+	infos, err := cpu.Info()
+	if err != nil {
+		return err
+	}
+	for _, info := range infos {
+		fmt.Fprintf(&text, "CPU: %s\n", info.ModelName)
+		break
+	}
+	err = os.WriteFile(path.Join("results", "info.md"), []byte(text.String()), 0666)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
