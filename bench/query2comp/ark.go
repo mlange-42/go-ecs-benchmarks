ddcf07d0dc52a600c595438ebb2553fa1ca1ac08
mlange-42 <martin_lange_@gmx.net>
2025-03-18 15:03:02 +0100

    Add Ark with registered filter

--- bench/query2comp/ark.go
+++ bench/query2comp/ark.go
@@ -26,3 +26,23 @@ func runArk(b *testing.B, n int) {
 		}
 	}
 }
+
+func runArkRegistered(b *testing.B, n int) {
+	world := ecs.NewWorld(1024)
+
+	mapper := ecs.NewMap2[comps.Position, comps.Velocity](&world)
+	for range n {
+		_ = mapper.NewEntity(&comps.Position{}, &comps.Velocity{X: 1, Y: 1})
+	}
+
+	filter := ecs.NewFilter2[comps.Position, comps.Velocity](&world).Register()
+
+	for b.Loop() {
+		query := filter.Query()
+		for query.Next() {
+			pos, vel := query.Get()
+			pos.X += vel.X
+			pos.Y += vel.Y
+		}
+	}
+}
--- bench/query2comp/run.go
+++ bench/query2comp/run.go
@@ -11,6 +11,7 @@ func Benchmarks() util.Benchmarks {
 			{Name: "Arche", F: runArche},
 			{Name: "Arche (cached)", F: runArcheRegistered},
 			{Name: "Ark", F: runArk},
+			{Name: "Ark (cached)", F: runArkRegistered},
 			{Name: "Donburi", F: runDonburi},
 			{Name: "ggecs", F: runGGEcs},
 			{Name: "uot", F: runUot},
